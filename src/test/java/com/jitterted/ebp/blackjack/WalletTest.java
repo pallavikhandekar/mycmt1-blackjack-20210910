9e71beeffb0183990c6158d98ec1de00b5ab597a
Ted M. Young <tedyoung@gmail.com>
2021-09-10 14:30:11 -0700

    TDD: Created Wallet with isEmpty query behavior.

---
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -0,0 +1,32 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class WalletTest {
+
+    @Test
+    public void newWalletIsEmpty() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThat(wallet.isEmpty())
+                .isTrue();
+    }
+
+    @Test
+    public void newWalletWhenAddMoneyThenIsNotEmpty() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(1);
+
+        assertThat(wallet.isEmpty())
+                .isFalse();
+    }
+
+
+    // AAA = arrange, act, assert
+    // GWT = given, when, then
+    // SEV = setup, execute, verify (Meszaros)
+
+}
